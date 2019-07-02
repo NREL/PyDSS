8367688584f3fce6e0e4cf4c9caf26ce102d393d
Aadil Latif <aadil.latif@nrel.gov>
2019-07-02 15:00:50 -0600

    Changes to simulation settings Changes to solve class to be able to but sub second simulations

--- PyDSS.py
+++ PyDSS.py
@@ -12,25 +12,27 @@ import os
 @click.option('--export_style', default='Single file', type=click.STRING,
               help='possible options "Single_file" and "Separate_files"')
 # Plot Settings
-@click.option('--create_dynamic_plots', default=True, type=click.BOOL, help='render dynamic plot using bokeh')
+@click.option('--create_dynamic_plots', default=False, type=click.BOOL, help='render dynamic plot using bokeh')
 
 # Simulation Settings
 @click.option('--start_year', default=2017, type=click.INT, help='Start year for the simulation study')
 @click.option('--start_day', default=1, type=click.INT, help='Start day for the simulation study')  # 156-163, 286-293
-@click.option('--end_day', default=3, type=click.INT, help='End day for the simulation study')
+@click.option('--start_time_min', default=840.6, type=click.FLOAT, help='Start time in minutes')  # 156-163, 286-293
+@click.option('--end_day', default=1, type=click.INT, help='End day for the simulation study')
+@click.option('--end_time_min', default=841.1, type=click.FLOAT, help='end time in minutes')  # 156-163, 286-293
 @click.option('--date_offset', default=1, type=click.INT, help='Date offset to be added')
-@click.option('--step_resolution_min', default=30, type=click.FLOAT, help='Time step resolution in minutes')
+@click.option('--step_resolution_sec', default=1/240, type=click.FLOAT, help='Time step resolution in seconds')
 @click.option('--max_control_iterations', default=15, type=click.INT, help='Maximum outer loop control iterations')
 @click.option('--error_tolerance', default=0.001, type=click.FLOAT, help='Error tolerance in per unit')
 @click.option('--simulation_type', default='Daily', type=click.STRING, help='possible modes "Daily" and "Snapshot"')
-@click.option('--active_project', default='BRPL', type=click.STRING, help='Name of project to run')
-@click.option('--active_scenario', default='peak_shaving', type=click.STRING, help='Project scenario to use')
-@click.option('--dss_file', default='Darbi_1.dss', type=click.STRING,
+@click.option('--active_project', default='PV_with_voltage_ridethrough', type=click.STRING, help='Name of project to run')
+@click.option('--active_scenario', default='smartPV', type=click.STRING, help='Project scenario to use')
+@click.option('--dss_file', default='test_network.dss', type=click.STRING,
               help='The main OpenDSS file')
 # Logger settings
 @click.option('--logging_level', default='DEBUG', type=click.STRING, help='possible options "DEBUG" and "INFO"')
 @click.option('--log_to_external_file', default=False, type=click.BOOL, help='Boolean variable ')
-@click.option('--display_on_screen', default=True, type=click.BOOL, help='Boolean variable')
+@click.option('--display_on_screen', default=False, type=click.BOOL, help='Boolean variable')
 @click.option('--clear_old_log_file', default=True, type=click.BOOL, help='Boolean variable')
 
 def RunSimulation(**kwargs):
@@ -48,9 +50,11 @@ def RunSimulation(**kwargs):
         'Project Path': r'C:\Users\alatif\Desktop\PyDSS-Projects',
         'Start Year': kwargs.get('start_year'),
         'Start Day': kwargs.get('start_day'),
+        'Start Time (min)': kwargs.get('start_time_min'),
         'End Day': kwargs.get('end_day'),
+        'End Time (min)': kwargs.get('end_time_min'),
         'Date offset' : kwargs.get('date_offset'),
-        'Step resolution (min)': kwargs.get('step_resolution_min'),
+        'Step resolution (sec)': kwargs.get('step_resolution_sec'),
         'Max Control Iterations': kwargs.get('max_control_iterations'),
         'Error tolerance': kwargs.get('error_tolerance'),
         'Simulation Type': kwargs.get('simulation_type'),
