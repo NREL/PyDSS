b6554eef784d4e172fb706ad285a7c6b40ac7d97
Aadil Latif <aadil.latif@nrel.gov>
2019-09-24 13:51:24 -0600

    Changes to visualization module

--- PyDSS/pyAnalyzer/dssPlots/dssVoltageDistance.py
+++ PyDSS/pyAnalyzer/dssPlots/dssVoltageDistance.py
@@ -1,17 +1,122 @@
+import matplotlib.pyplot as plt
+import seaborn as sbn
 import pandas as pd
+import pathlib
 import os
 
 class Plot:
 
-    def __init__(self, resultPath):
-        #print(resultPath)
-        self.__result_files = [x for x in list(os.walk(resultPath))[0][2] if x.endswith('.csv')]
-        self.__result_dict = {}
-        for result in self.__result_files:
-            if result != 'event_log.csv':
-                self.__result_dict[result.replace('.csv', '')] = pd.read_csv(os.path.join(resultPath, result), index_col=0)
-                #print(self.__result_dict[result.replace('.csv', '')])
+    def __init__(self, visualization_args, simulation_results):
+        self.simulation_results = simulation_results
+        self.visualization_args = visualization_args
+
+        if visualization_args['Voltage_sag']:
+            self.__create_voltage_sag_plot(visualization_args, simulation_results)
+
         return
 
-    def get_results(self):
-        return self.__result_dict
+    def __create_voltage_sag_plot(self, visualization_args, simulation_results):
+        plot_settings = visualization_args['Voltage_sag_settings']
+        width = visualization_args['Global_width'] if visualization_args['Global_override'] else  plot_settings['Width']
+        height = visualization_args['Global_height'] if visualization_args['Global_override'] else  plot_settings['Height']
+        dpi = visualization_args['Global_DPI'] if visualization_args['Global_override'] else  plot_settings['DPI']
+        first_scenario = True
+        simulation_key = None
+        fig = None
+        ax = None
+        colors = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+        c = 0
+        lines = []
+        labels = []
+
+        for simulation_key, value in simulation_results.items():
+            if visualization_args['Plotting_mode'] == "Separate" or (
+                    visualization_args['Plotting_mode'] == "Single" and first_scenario):
+                fig, ax = self.__create_figure(width, height, dpi, 'Distance [Mi]', 'Voltage magnitude [p.u.]')
+            simulation_settings, result_obj = value
+            ExportFolder = os.path.join(simulation_settings["Project Path"], simulation_settings["Active Project"],
+                "Exports", simulation_settings["Active Scenario"], "Plots")
+            ExportFolder2 = os.path.join(simulation_settings["Project Path"], simulation_settings["Active Project"],
+                                        "Exports", simulation_settings["Active Scenario"], "Scenario Comparison Plots")
+            results = result_obj.get_results()
+            relevant_key = self.__check_result_existance(results, 'Buses-puVmagAngle')
+            relevant_dist_key = self.__check_result_existance(results, 'Buses-Distance')
+            if relevant_key != None and relevant_dist_key != None:
+                plotdata = self.__filter_DF_magntitudes(results[relevant_key], plot_settings['Frequency'],
+                                                        plot_settings['Simulation_mode'])
+                plotdata = plotdata.loc[plot_settings['Time']]
+                distdata = results[relevant_dist_key].loc[plot_settings['Time']]
+                distances = []
+                for c1 in [x.split(" ")[0] for x in plotdata.index]:
+                    distances.append(distdata.loc[c1 + ' [Mi]'])
+                voltages = plotdata.values
+
+                if visualization_args['Plotting_mode'] == "Separate" or (
+                        visualization_args['Plotting_mode'] == "Single" and first_scenario):
+                    if plot_settings['display_limits']:
+                        b1 = ax.axhspan(plot_settings['UV'][0], plot_settings['OV'][0], alpha=0.05,
+                                        color='darkolivegreen', label='normal region')
+                        b2 = ax.axhspan(plot_settings['UV'][0], plot_settings['UV'][1], alpha=0.05,
+                                        color='darkgoldenrod', label='control region')
+                        b3 = ax.axhspan(plot_settings['UV'][1], plot_settings['UV'][2], alpha=0.05, color='darkorange',
+                                        label='curtailment region')
+                        b4 = ax.axhspan(plot_settings['UV'][2], 0.0, alpha=0.1, color='darkred', label='trip region')
+                        b5 = ax.axhspan(plot_settings['OV'][0], plot_settings['OV'][1], alpha=0.05,
+                                        color='darkgoldenrod')
+                        b6 = ax.axhspan(plot_settings['OV'][1], plot_settings['OV'][2], alpha=0.05, color='darkorange')
+                        b7 = ax.axhspan(plot_settings['OV'][2], 1.2, alpha=0.1, color='darkred')
+
+                        lines.extend([b1, b2, b3, b4])
+                        labels.extend(['normal region', 'control region', 'curtailment region', 'trip region'])
+                        for b, h in zip([b1, b2, b3, b4, b5, b6, b7], ['\\\\', 'x', '//', '+', 'x', '//', '+']):
+                            b.set_hatch(h)
+                yrange = (max(voltages) - min(voltages)) * 0.05
+
+                ax.set_ylim(min(voltages) - yrange, max(voltages) + yrange)
+                line = ax.scatter(distances, voltages)
+                lines.append(line)
+                labels.append(simulation_key)
+                leg = ax.legend(lines, labels, frameon=False, prop={'size': plot_settings['Legend_font_size']})
+                for lh in leg.legendHandles:
+                    lh.set_alpha(0.6)
+                if plot_settings['Grid']:
+                    ax.grid(color='lightgrey', linestyle='-')
+                fig.tight_layout()
+                if visualization_args['Plotting_mode'] == "Separate":
+                    self.save_file(fig, ExportFolder, simulation_key, 'voltage_sag', visualization_args['FileType'])
+
+            first_scenario = False
+            c+=1
+        if visualization_args['Plotting_mode'] == "Single":
+            self.save_file(fig, ExportFolder2, simulation_key, 'voltage_sag', visualization_args['FileType'])
+        return
+
+    def __check_result_existance(self, results, result_key):
+        relevant_key = None
+        for key in results.keys():
+            if key.startswith(result_key):
+                relevant_key = key
+                break
+        return relevant_key
+
+    def __filter_DF_magntitudes(self, data, frequecy, simulation_mode):
+        data = data[data['frequency'] == frequecy]
+        data = data[data['Simulation mode'] == simulation_mode]
+        data = data[data.columns[2:]]
+        data = data[data.columns[::2]]
+        data = data.loc[:, (data != 0).any(axis=0)]
+        return data
+
+    def __create_figure(self, width, height, dpi, xlabel, ylabel):
+        fig = plt.figure(figsize=(width, height), dpi=dpi)
+        ax = fig.add_subplot(111)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        return fig, ax
+
+    def save_file(self, figure, path, scenario,plot_type, extension):
+        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+        figpath = '{}\{}-{}.{}'.format(path, scenario, plot_type, extension)
+        figure.savefig(figpath)
+        print("File saved: {}".format(figpath))
+        return
--- PyDSS/pyAnalyzer/dssPlots/dssXYplot.py
+++ PyDSS/pyAnalyzer/dssPlots/dssXYplot.py
@@ -1,117 +1,203 @@
 import matplotlib.pyplot as plt
 import seaborn as sbn
 import pandas as pd
+import numpy as np
 import pathlib
 import os
 
+
 class Plot:
+    def __init__(self, plot_type, plot_data):
+        colormaps = [
+            'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+            'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+            'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn'
+        ]
+        X_Y_dataframes = []
+        scenario_number = 0
+        plot_settings = None
+        visualization_args = None
+        for scenario in plot_data:
+            plot_settings = plot_data[scenario]['Plot_settings']
+            scenario_settings = plot_data[scenario]['Scenario_settings']
+            visualization_args = plot_data[scenario]['Visualization_settings']
+            height = plot_settings['Height']
+            path_seperate, path_single = self.__get_paths(scenario_settings)
+            if visualization_args['Plotting_mode'] == "Separate":
+                self.legend_lines = []
+                self.legend_labels = []
+            create_new_plot = visualization_args['Plotting_mode'] == "Separate" or (
+                    visualization_args['Plotting_mode'] == "Single" and scenario_number == 0)
+            elm_Class = plot_settings['Class'] if 'Class' in plot_settings else ''
+            color = colormaps[scenario_number % len(colormaps)]
+            if plot_type == 'Voltage_Loading':
+                X_Y_dataframes.append(self.__get_voltage_loading_data(
+                        plot_data=plot_data, scenario=scenario, plot_settings=plot_settings, elm_Class=elm_Class
+                ))
+            if plot_type == 'Load_Generation':
+                X_Y_dataframes.append(self.__get_load_generation_data(
+                    plot_data=plot_data, scenario=scenario, plot_settings=plot_settings, elm_Class=elm_Class
+                ))
+            if visualization_args['Plotting_mode'] == "Separate":
+                columns = X_Y_dataframes[-1].columns
+                X = X_Y_dataframes[-1][columns[0]].values
+                Y = X_Y_dataframes[-1][columns[1]].values
+                sbn.set(style="white")
+                joint_plot = sbn.jointplot(X, Y, kind=plot_settings["Kind"], height=height, space=0, cmap=color)
+                joint_plot.fig.axes[0].set_ylabel('Line loading [%]')
+                joint_plot.fig.axes[0].set_xlabel('Voltage magnitude [p.u.]')
+                joint_plot.fig.tight_layout()
+                self.save_file(joint_plot.fig, path_seperate, scenario, '{}-VLplot'.format(plot_settings['Class']),
+                               visualization_args['FileType'])
+            scenario_number += 1
+        if visualization_args['Plotting_mode'] == "Single":
+            df = pd.concat(X_Y_dataframes)
+            columns =df.columns
+            self.multivariateGrid(columns[0], columns[1], columns[2], df=df, kind=plot_settings["Kind"])
+            self.save_file(plt.gcf(), path_single, scenario, '{}-VLplot'.format(plot_settings['Class']),
+                                   visualization_args['FileType'])
 
-    def __init__(self, visualization_args, simulation_results):
-        self.simulation_results = simulation_results
-        self.visualization_args = visualization_args
-
-        if visualization_args['Voltage_Loading']:
-            self.__create_voltage_loading_plot(visualization_args, simulation_results)
         return
 
-    def __create_voltage_loading_plot(self, visualization_args, simulation_results):
-        plot_settings = visualization_args['Loading_PDF_settings']
-        width = visualization_args['Global_width'] if visualization_args['Global_override'] else plot_settings['Width']
-        height = visualization_args['Global_height'] if visualization_args['Global_override'] else plot_settings[
-            'Height']
-        dpi = visualization_args['Global_DPI'] if visualization_args['Global_override'] else plot_settings['DPI']
-        first_scenario = True
-        simulation_key = None
-        simulation_settings = None
-        for simulation_key, value in simulation_results.items():
-            if visualization_args['Plotting_mode'] == "Separate" or (
-                    visualization_args['Plotting_mode'] == "Single" and first_scenario):
-                fig = plt.figure(figsize=(width, height), dpi=dpi)
-                ax = fig.add_subplot(111)
-                ax.set_xlabel('Line loading [%]')
-                ax.set_ylabel('Voltage magnitude [p.u.]')
+    def __get_load_generation_data(self, plot_data, scenario, plot_settings, elm_Class):
+        key_powers = self.__get_key(plot_data[scenario]['Data'], 'Powers', elm_Class)
+        generation_kw = self.__get_data(plot_data[scenario]['Data'][key_powers])
+        key_powers = self.__get_key(plot_data[scenario]['Data'], 'Loads-Powers', None)
+        load_kw = self.__get_data(plot_data[scenario]['Data'][key_powers])
+        load = load_kw.sum(axis=1).values
+        generation = (-generation_kw.sum(axis=1)).values
+        parsed_data = pd.DataFrame(np.matrix([load, generation]).T, columns=[
+            'Total active power demand [kW]',
+            'Total generation from "{}" [kW]'.format(plot_settings['Class'])])
+        parsed_data['kind'] = scenario
+        return parsed_data
 
-            simulation_settings, result_obj = value
-            results = result_obj.get_results()
-            current_file_exist = False
-            normamp_file_exist = False
-            voltage_file_exist = False
-            relevant_key_voltage = None
-            relevant_key_normamps = None
-            relevant_key_current = None
-            for key in results.keys():
-                if key.startswith('Lines-normamps'):
-                    normamp_file_exist = True
-                    relevant_key_normamps = key
-                    break
 
-            for key in results.keys():
-                if key.startswith('Lines-CurrentsMagAng'):
-                    current_file_exist = True
-                    relevant_key_current = key
-                    break
+    def __get_voltage_loading_data(self, plot_data, scenario, plot_settings, elm_Class):
+        key = self.__get_key(plot_data[scenario]['Data'], 'CurrentsMagAng', plot_settings['Class'])
+        currents = self.__get_data(plot_data[scenario]['Data'][key])
 
-            for key in results.keys():
-                if key.startswith('Buses-puVmagAngle'):
-                    voltage_file_exist = True
-                    relevant_key_voltage = key
-                    break
+        key_normamps = self.__get_key(plot_data[scenario]['Data'], 'normamps', plot_settings['Class'])
+        norm_amps = self.__get_data(plot_data[scenario]['Data'][key_normamps])
 
-            if current_file_exist and normamp_file_exist and voltage_file_exist:
-                norm_amps = results[relevant_key_normamps]
-                currents = results[relevant_key_current]
-                currents = currents[currents.columns[2:]]
-                currents = currents[currents.columns[::2]]
-                currents = currents.loc[:, (currents != 0).any(axis=0)]
-                current_cols = [x.split(' ')[0] for i, x in enumerate(currents.columns)]
-                loading = pd.DataFrame()
-                for c1, c2 in zip(currents.columns, current_cols):
-                    loading[c1] = currents[c1] / norm_amps[c2 + ' [Amp]'] * 100
+        key = self.__get_key(plot_data[scenario]['Data'], 'Buses-puVmagAngle', None)
+        voltages = self.__get_data(plot_data[scenario]['Data'][key])
 
-                voltages = results[relevant_key_voltage]
-                voltages = voltages[voltages['frequency'] == plot_settings['Frequency']]
-                voltages = voltages[voltages['Simulation mode'] == plot_settings['Simulation_mode']]
-                voltages = voltages[voltages.columns[2:]]
-                voltages = voltages[voltages.columns[::2]]
-                voltages = voltages.loc[:, (voltages != 0).any(axis=0)]
-                voltage_cols = [x.replace(' [V:pu]', '') for x in voltages.columns]
-                print(loading)
-                print(voltages)
-                break
+        if elm_Class == 'Transformers':
+            key = self.__get_key(plot_data[scenario]['Data'], 'bus', plot_settings['Class'])
+            line_bus = self.__get_data(plot_data[scenario]['Data'][key])
+        else:
+            key = self.__get_key(plot_data[scenario]['Data'], 'bus1', plot_settings['Class'])
+            line_bus = self.__get_data(plot_data[scenario]['Data'][key])
 
-                plotdata = loading.values.flatten()
+        current_cols = [x.split(' ')[0] for i, x in enumerate(currents.columns)]
+        loading = pd.DataFrame()
+        line_voltages = pd.DataFrame()
+        for c1, c2 in zip(currents.columns, current_cols):
+            loading[c1] = currents[c1] / norm_amps[c2 + ' [Amp]'] * 100
+            busInfo = str(line_bus[c2].tolist()[0])
+            busInfo = busInfo.split('.')
+            bus = busInfo[0]
+            phases = busInfo[1:]
+            for p in phases:
+                col_name = '{} ph:{} [V:pu]'.format(bus, p)
+                if col_name in voltages:
+                    line_voltages[c1] = voltages[col_name]
 
-                sbn.distplot(
-                    plotdata,
-                    ax=ax,
-                    label=simulation_key,
-                    hist_kws=dict(alpha=plot_settings['Alpha']),
-                    norm_hist=False,
-                    kde=True,
-                )
+        common_cols = list(set(loading.columns).intersection(set(line_voltages.columns)))
+        loading = loading[common_cols].values.flatten()
+        line_voltages = line_voltages[common_cols].values.flatten()
 
-                ax.legend(prop={'size': plot_settings['Legend_font_size']})
-                if plot_settings['Grid']:
-                    ax.grid(color='lightgrey', linestyle='-')
-                fig.tight_layout()
-                if visualization_args['Plotting_mode'] == "Separate":
-                    ExportFolder = os.path.join(
-                        simulation_settings["Project Path"],
-                        simulation_settings["Active Project"],
-                        "Exports",
-                        simulation_settings["Active Scenario"],
-                        "Plots"
-                    )
-                    self.save_file(fig, ExportFolder, simulation_key, 'loadingPDF', visualization_args['FileType'])
+        parsed_data = pd.DataFrame(np.matrix([line_voltages, loading]).T, columns=[
+            'Voltage magnitude [p.u.]',
+            '{} loading [%]'.format(plot_settings['Class'][:-1])])
+        parsed_data['kind'] = scenario
+        return parsed_data
 
-            if visualization_args['Plotting_mode'] == "Single":
-                ExportFolder = os.path.join(
-                    simulation_settings["Project Path"],
-                    simulation_settings["Active Project"],
-                    "Exports",
-                    simulation_settings["Active Scenario"],
-                    "Scenario Comparison Plots"
-                )
-                self.save_file(fig, ExportFolder, simulation_key, 'loadingPDF', visualization_args['FileType'])
-            first_scenario = False
+    def __get_data(self, data, even=True):
+        if isinstance(data, tuple):
+            data_even, data_odd = data
+            if not even:
+                data_even = data_odd
+        else:
+            data_even = data
+        return data_even
+
+    def __get_key(self, data_dict, filetag, elm_class):
+        if elm_class == None:
+            key = [x for x in data_dict if x.startswith('{}-'.format(filetag))][0]
+        else:
+            key = [x for x in data_dict if x.startswith('{}-{}-'.format(elm_class, filetag))][0]
+        return key
+
+    def __get_paths(self, simulation_settings):
+        path_seperate = os.path.join(simulation_settings["Project Path"], simulation_settings["Active Project"],
+                                     "Exports", simulation_settings["Active Scenario"], "Plots")
+        path_single = os.path.join(simulation_settings["Project Path"], simulation_settings["Active Project"],
+                                   "Exports", simulation_settings["Active Scenario"], "Scenario Comparison Plots")
+        return path_seperate, path_single
+
+    def save_file(self, figure, path, scenario, plot_type, extension):
+        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+        figpath = '{}\{}-{}-XY.{}'.format(path, scenario, plot_type, extension)
+        figure.savefig(figpath)
+        plt.close(figure)
+        print("File saved: {}".format(figpath))
         return
+
+
+    def multivariateGrid(self, col_x, col_y, col_k, df, k_is_color=False, scatter_alpha=.1, kind="scatter"):
+        from matplotlib.lines import Line2D
+
+        def colored_scatter(x, y, c=None):
+            def scatter(*args, **kwargs):
+                args = (x, y)
+                if c is not None:
+                    kwargs['c'] = c
+                kwargs['alpha'] = scatter_alpha
+                plt.scatter(*args, **kwargs)
+            return scatter
+
+        def colored_contour(x, y,  c=None):
+            def contourf(*args, **kwargs):
+                sbn.kdeplot(x, y, shade=True, shade_lowest=False)
+                if c is not None:
+                    kwargs['c'] = c
+                kwargs['alpha'] = scatter_alpha
+            return contourf
+
+        g = sbn.JointGrid(
+            x=col_x,
+            y=col_y,
+            data=df,
+        )
+        legends = []
+        custom_lines = []
+        colors =plt.cm.tab10.colors
+
+        c = 0
+        for name, df_group in df.groupby(col_k):
+            if k_is_color:
+                color = name
+            legends.append(name)
+            custom_lines.append(Line2D([0], [0], color=colors[c], lw=4))
+
+            g.plot_joint(
+                colored_contour(df_group[col_x], df_group[col_y], colors[c]) if kind == 'kde' else
+                colored_scatter(df_group[col_x], df_group[col_y], colors[c]),
+            )
+            sbn.distplot(
+                df_group[col_x].values,
+                ax=g.ax_marg_x,
+                color=colors[c],
+            )
+            sbn.distplot(
+                df_group[col_y].values,
+                ax=g.ax_marg_y,
+                color=colors[c],
+                vertical=True
+            )
+            c += 1
+
+        plt.legend(custom_lines, legends)
+
