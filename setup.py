2cc245daa9639d3c7d91b9980b0879525f719268
Aadil Latif <32109280+AadilLatif@users.noreply.github.com>
2021-06-02 17:20:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,8 @@ def find_version(*file_paths):
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
                               version_file, re.M)
     if version_match:
-        return version_match.group(1)
+        ver =  version_match.group(1)
+        return ".".join(ver.split(".")[:-1])
     raise RuntimeError("Unable to find version string.")
 
 setup(name='dsspy',
@@ -35,6 +36,7 @@ setup(name='dsspy',
       url='http://www.github.com/nrel/pydss',
       packages=find_packages(),
       install_requires=requirements,
+      include_package_data=True,
       package_data={'PyDSS': ['*.toml']},
         entry_points={
             "console_scripts": [
