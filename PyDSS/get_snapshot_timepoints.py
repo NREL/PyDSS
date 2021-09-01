02f32387b9b4ea71d35631051952c70cacec6c0b
ann-sherin <sherin.abraham92@gmail.com>
2021-09-01 15:10:22 -0600

    get snapshot timepoints

---
+++ PyDSS/get_snapshot_timepoints.py
@@ -0,0 +1,39 @@
+from PyDSS.utils.simulation_utils import create_loadshape_pmult_dataframe_for_simulation
+from PyDSS.reports.reports import logger
+import pandas as pd
+
+
+def get_snapshot_timepoint(dss, options):
+    pv_systems = dss.PVsystems.AllNames()
+    if pv_systems is None:
+        logger.info("No PVSystems are present")
+    pv_profiles_df = pd.DataFrame()
+    for pv_name in pv_systems:
+        dss.PVsystems.Name(pv_name)
+        pmpp = float(dss.Properties.Value('Pmpp'))
+        profile_name = dss.Properties.Value('yearly')
+        dss.LoadShape.Name(profile_name)
+        pv_profiles_df[pv_name] = create_loadshape_pmult_dataframe_for_simulation(options) * pmpp
+
+    loads = dss.Loads.AllNames()
+    if loads is None:
+        logger.info("No Loads are present")
+    load_profiles_df = pd.DataFrame()
+    for load_name in loads:
+        dss.Loads.Name(load_name)
+        kw = float(dss.Properties.Value('kW'))
+        profile_name = dss.Properties.Value('yearly')
+        dss.LoadShape.Name(profile_name)
+        load_profiles_df[load_name] = create_loadshape_pmult_dataframe_for_simulation(options) * kw
+
+    aggregate_profiles = pd.DataFrame()
+    aggregate_profiles['Load'] = load_profiles_df.sum(axis=1)
+    aggregate_profiles['PV'] = pv_profiles_df.sum(axis=1)
+    aggregate_profiles['PV to Load Ratio'] = aggregate_profiles['PV'] / aggregate_profiles['Load']
+    aggregate_profiles['PV-Load'] = aggregate_profiles['PV'] - aggregate_profiles['Load']
+
+    timepoints = pd.DataFrame({'Timepoints': aggregate_profiles.idxmax().T})
+    timepoints.index = 'Max ' + timepoints.index
+    timepoints.loc['Min Load'] = aggregate_profiles['Load'].idxmin()
+    return timepoints
+
