d4dd60384e5a7ed1ca6ee9d8f8083b43690d4ef3
Aadil Latif <aadil.latif@gmail.com>
2021-10-26 16:20:33 -0600

    Creating substation model

--- PyDSS/api/src/app/pydss.py
+++ PyDSS/api/src/app/pydss.py
@@ -4,6 +4,8 @@ from multiprocessing import current_process
 import inspect
 from queue import Empty
 
+import opendssdirect as dss
+
 from PyDSS.dssInstance import OpenDSS
 from PyDSS.api.src.app.model_creator.Scenario_generator import PyDSS_Model
 from PyDSS.api.src.web.parser import restructure_dictionary
@@ -16,6 +18,7 @@ from zipfile import ZipFile
 import re
 import shutil
 import toml
+import ast
 
 logger = logging.getLogger(__name__)
 
@@ -241,17 +244,87 @@ class PyDSS:
             self.notify(f"Error fetching PyDSS project!", log_level=logging.ERROR)
             return None
 
-    def update_project(self, 
-                file_uuid,
-                load_mw,
-                load_mvar,
-                der_mw,
-                der_mvar,
-                motor_d: 0,
-                pvcategory= {"ieee-2018-catI": 0, "ieee-2018-catII": 0, "ieee-2018-catIII": 0, "ieee-2003": 0},
-                dynamic= True,
-                update_model=True
-                ):
+    def create_substation(self, base_path, feeder_models, source_voltage):
+        substation_kw = 0
+        substation_kvar = 0
+        master_dss_file = "master.dss"
+        redirect_lines = []
+        feeder_bus = []
+        feeder_kv = []
+        all_voltage_bases = []
+        for feeder in feeder_models:
+            feeder_path = os.path.join(base_path, feeder, master_dss_file)
+            dss.run_command(f"compile {feeder_path}")
+            dss.run_command(f"solve")
+            P, Q = dss.Circuit.TotalPower()
+            substation_kw += P
+            substation_kvar += Q
+
+            f = open(feeder_path, "r")
+            lines = f.readlines()
+            f.close()
+            for line in lines:
+
+                if "voltagebases" in line.lower():
+                    redirect_lines.append(line)
+                    sStrings = line.split(" ")
+                    for sString in sStrings:
+                        if "voltagebases=" in sString.lower():
+                            vbase = sString.replace("voltagebases=", '')
+                            vbase = ast.literal_eval(vbase)
+                            all_voltage_bases.extend(vbase)
+
+                if "redirect" in line.lower():
+                    sStrings = line.split(" ")
+                    nLine = f"redirect {feeder}/{sStrings[1]}"
+                    redirect_lines.append(nLine)
+
+                if "circuit" in line.lower():
+                    sStrings = line.split(" ")
+                    for sString in sStrings:
+                        if "bus1" in sString.lower():
+                            feeder_bus.append(sString.lower().replace("bus1", "").split(".")[0])
+                        if "basekv" in sString.lower():
+                            feeder_kv.append(sString.lower().replace("basekv", ""))
+
+        master_file_path = os.path.join(base_path, master_dss_file)
+        f = open(master_file_path, "w")
+        f.write("clear\n")
+        f.write(f"new circuit.substation basekv={source_voltage} pu=1.03 phases=3 bus1=source_bus")
+
+        substation_kva = (substation_kw**2 + substation_kvar**2)**0.5
+
+        tr = f"New Transformer.ABC phases=3 Windings=2 xhl=5"
+        wdg1 = f" wdg=1 bus=source_bus conn=delta kV={source_voltage} kva={substation_kva} %r=0.1"
+        wdg2 = f" wdg=2 bus=substation_bus conn=wye kV=12.47 kva={substation_kva} %r=0.1\n"
+        f.write(tr + wdg1 + wdg2)
+
+        for i, kv, bus in zip(range(len(feeder_kv)), feeder_kv, feeder_bus):
+            f.write(f"new reactor.switch_{i} phases=3 Bus1=substation_bus Bus2={bus} R=0 X=0.000001 kv=12.47")
+            assert kv == "12.47", "There is a voltage mismatch between substation and feeder models"
+
+        for line in redirect_lines:
+            f.write(line + "\n")
+
+        f.write(f"set voltagebases={list(set(all_voltage_bases))}\n")
+        f.write(f"calcvoltagebases\n")
+        f.write(f"solve\n")
+        f.close()
+        return master_dss_file
+
+    def update_project(self,
+                       file_uuids,
+                       load_mw,
+                       load_mvar,
+                       der_mw,
+                       der_mvar,
+                       source_voltage,  # @KAPIL NEED TO GET THIS INFORMATION FROM COSIM LAUNCHER
+                       motor_d: 0,
+                       pvcategory={"ieee-2018-catI": 0, "ieee-2018-catII": 0, "ieee-2018-catIII": 0, "ieee-2003": 0},
+                       dynamic= True,
+                       update_model=True,
+
+                       ):
 
         """ Grab file and metadata info of case file """
         tmp_folder = os.path.join(os.path.dirname(__name__), '..', 'tmp')
@@ -260,114 +333,120 @@ class PyDSS:
         if not os.path.exists(self.tmp_folder):
             os.mkdir(tmp_folder)
 
-        folder_name = file_uuid + 'distribution_federate'
-        case_file_url = f'{self.data_service_url}/case_files/uuid/{file_uuid}'
+        feeder_folders = []
+        for file_uuid in file_uuids:
 
-        """ Creating a project folder and removing if already exists"""
-        dss_dump = os.path.join(tmp_folder, folder_name)
+            folder_name = file_uuid + 'distribution_federate'
+            case_file_url = f'{self.data_service_url}/case_files/uuid/{file_uuid}'
 
-        if not os.path.exists(dss_dump):
-            os.mkdir(dss_dump)
-        else:
-            shutil.rmtree(dss_dump)
-            os.mkdir(dss_dump)
-
-        """ Retrieve the file url from the BES Data API """
-        self.notify(f"URL for PYDSS case file {case_file_url}")
-        file_response = send_sync_request(case_file_url, 'GET', body=None)
-        file_data = json.loads(file_response.data.decode('utf-8'))
-
-        if file_response.status == HTTPStatus.OK:
-            file_url = file_data['file_url']
-            self.notify(f"URL to download the file> {file_url}")
-            file_format = file_data['format']
-            
-
-            """ Retrieve the file from AWS and store in the tmp directory """
-            data_response = send_sync_request(file_url, 'GET')
-            if data_response.status == HTTPStatus.OK:
-                file = os.path.join(dss_dump, f'{file_uuid}.{file_format}')
-                with open(file, 'wb') as f:
-                    f.write(data_response.data)
-
-                """ Unzip the files """
-                zip_path = os.path.join(dss_dump, f'{file_uuid}.{file_format}')
-                if zip_path.endswith('.zip'):
-                    with ZipFile(zip_path, 'r') as zipObj:
-                        zipObj.extractall(path=dss_dump)
-                    os.remove(zip_path)
-
-                    """ create a new PyDSS project here"""
-        
-                    scenario = "scenario1"
-                    project = "project1"
-
-                    try:
-                        self.create_project(
-                            dss_dump,
-                            project,
-                            scenario,
-                            dss_dump,
-                            file_data['case']['master_file']
-                        )
-                        
-
-                        """ update the model with new settings """
-                        model_modifier = PyDSS_Model(os.path.join(dss_dump,project))
-                        pmult, qmult = model_modifier.create_new_scenario(
-                            scenario, 
-                            {
-                                "Lp": load_mw,
-                                "Lq": load_mvar,
-                                "PVp": der_mw,
-                                "PVq": der_mvar,
-                                "Mtr": motor_d
-                            },
-                            {
-                                "new_master": "new_" + file_data['case']['master_file'],
-                                "master": file_data['case']['master_file'],
-                                "load": 'Loads.dss', # TODO: needs to come from metadata
-                                "motor": 'Motors_new.dss',
-                                "PVsystem": 'PVSystems_new.dss',
-                            },
-                            isSubstation=True, # TODO: needs to come from metadata
-                            pvstandards = pvcategory,
-                            dynamic = True
-                        )
-
-                        powerflow_options = {
-                            'pydss_project': dss_dump,
-                            'active_project': project,
-                            'active_scenario': scenario,
-                            'master_dss_file': file_data['case']['master_file']
-                        }
-
-                     
-                    except Exception as e:
-                        logger.error(f'Error updating the PyDSS project >> {e}')
-                        self.notify(f"Error updating the PyDSS project >> {e}", log_level=logging.ERROR)
-                        return {}
-                    
-                    #TODO: validate pydss project skeleton
-
-                    return {
-                        "powerflow_options": powerflow_options,
-                        "mw_multiplier": pmult,
-                        "mvar_multiplier": qmult
-                    }
-                else:
-                    logger.error(f'OpenDSS model is not a zip file')
-                    self.notify(f"OpenDSS model is not a zip file", log_level=logging.ERROR)
-                    return {}
+            """ Creating a project folder and removing if already exists"""
+            dss_dump = os.path.join(tmp_folder, folder_name)
+            feeder_folders.append(folder_name)
+            if not os.path.exists(dss_dump):
+                os.mkdir(dss_dump)
             else:
-                logger.error(f'Error fetching data from AWS S3')
-                self.notify(f"Error fetching data from AWS S3", log_level=logging.ERROR)
+                shutil.rmtree(dss_dump)
+                os.mkdir(dss_dump)
+
+            """ Retrieve the file url from the BES Data API """
+            self.notify(f"URL for PYDSS case file {case_file_url}")
+            file_response = send_sync_request(case_file_url, 'GET', body=None)
+            file_data = json.loads(file_response.data.decode('utf-8'))
+
+            if file_response.status == HTTPStatus.OK:
+                file_url = file_data['file_url']
+                self.notify(f"URL to download the file> {file_url}")
+                file_format = file_data['format']
+
+                """ Retrieve the file from AWS and store in the tmp directory """
+                data_response = send_sync_request(file_url, 'GET')
+                if data_response.status == HTTPStatus.OK:
+                    file = os.path.join(dss_dump, f'{file_uuid}.{file_format}')
+                    with open(file, 'wb') as f:
+                        f.write(data_response.data)
+
+                    """ Unzip the files """
+                    zip_path = os.path.join(dss_dump, f'{file_uuid}.{file_format}')
+                    if zip_path.endswith('.zip'):
+                        with ZipFile(zip_path, 'r') as zipObj:
+                            zipObj.extractall(path=dss_dump)
+                        os.remove(zip_path)
+
+                        """ create a new PyDSS project here"""
+
+                    else:
+                        logger.error(f'OpenDSS model is not a zip file')
+                        self.notify(f"OpenDSS model is not a zip file", log_level=logging.ERROR)
+                        return {}
+                else:
+                    logger.error(f'Error fetching data from AWS S3')
+                    self.notify(f"Error fetching data from AWS S3", log_level=logging.ERROR)
+                    return {}
+
+            else:
+                logger.error(f'Error fetching distribution models!')
+                self.notify(f"Error fetching distribution models!", log_level=logging.ERROR)
                 return {}
 
-        else:
-            logger.error(f'Error fetching distribution models!')
-            self.notify(f"Error fetching distribution models!", log_level=logging.ERROR)
+
+        master_file = self.create_substation(tmp_folder, feeder_folders, source_voltage)
+
+        scenario = "scenario1"
+        project = "project1"
+        try:
+            self.create_project(
+                dss_dump,
+                project,
+                scenario,
+                dss_dump,
+                master_file, #file_data['case']['master_file']
+            )
+
+            """ update the model with new settings """
+            model_modifier = PyDSS_Model(os.path.join(dss_dump, project))
+            pmult, qmult = model_modifier.create_new_scenario(
+                scenario,
+                {
+                    "Lp": load_mw,
+                    "Lq": load_mvar,
+                    "PVp": der_mw,
+                    "PVq": der_mvar,
+                    "Mtr": motor_d
+                },
+                {
+                    "new_master": "new_" + master_file, #file_data['case']['master_file'],
+                    "master": master_file, #file_data['case']['master_file'],
+                    "load": 'Loads.dss',  # TODO: needs to come from metadata
+                    "motor": 'Motors_new.dss',
+                    "PVsystem": 'PVSystems_new.dss',
+                },
+                isSubstation=True,  # TODO: needs to come from metadata
+                pvstandards=pvcategory,
+                dynamic=True
+            )
+
+            powerflow_options = {
+                'pydss_project': dss_dump,
+                'active_project': project,
+                'active_scenario': scenario,
+                'master_dss_file': master_file, #file_data['case']['master_file']
+            }
+
+
+        except Exception as e:
+            logger.error(f'Error updating the PyDSS project >> {e}')
+            self.notify(f"Error updating the PyDSS project >> {e}", log_level=logging.ERROR)
             return {}
+
+        # TODO: validate pydss project skeleton
+
+        return {
+            "powerflow_options": powerflow_options,
+            "mw_multiplier": pmult,
+            "mvar_multiplier": qmult
+        }
+
+
 
     def create_project(self, 
         project_path,
