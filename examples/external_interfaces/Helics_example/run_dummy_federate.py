907bf5d4b2799664275f97d643bbc5b291d1af3b
Latif <alatif@nrel.gov>
2022-03-22 09:49:56 -0600

    dsaf

--- examples/external_interfaces/Helics_example/run_dummy_federate.py
+++ examples/external_interfaces/Helics_example/run_dummy_federate.py
@@ -13,16 +13,14 @@ helicsversion = h.helicsGetVersion()
 # Create broker #
 broker = h.helicsCreateBroker("zmq", "", initstring)
 isconnected = h.helicsBrokerIsConnected(broker)
-
+print(broker, isconnected)
 # Create Federate Info object that describes the federate properties #
 fedinfo = h.helicsCreateFederateInfo()
-
+print(fedinfo)
 # Set Federate name #
 h.helicsFederateInfoSetCoreName(fedinfo, "Test Federate")
-
 # Set core type from string #
 h.helicsFederateInfoSetCoreTypeFromString(fedinfo, "zmq")
-
 # Federate init string #
 h.helicsFederateInfoSetCoreInitString(fedinfo, fedinitstring)
 
@@ -30,22 +28,21 @@ h.helicsFederateInfoSetCoreInitString(fedinfo, fedinitstring)
 # HELICS minimum message time interval is 1 ns and by default
 # it uses a time delta of 1 second. What is provided to the
 # setTimedelta routine is a multiplier for the default timedelta.
-
 # Set one second message interval #
 h.helicsFederateInfoSetTimeProperty(fedinfo, h.helics_property_time_delta, deltat)
 #h.helicsFederateInfoSetIntegerProperty(fedinfo, h.helics_property_int_log_level, 20)
 # Create value federate #
 vfed = h.helicsCreateValueFederate("Test Federate", fedinfo)
-
+print(vfed)
 # Register the publication #
 pub1 = h.helicsFederateRegisterGlobalTypePublication(vfed, "test.load1.power", "double", "kW")
 pub2 = h.helicsFederateRegisterGlobalTypePublication(vfed, "test.feederhead.voltage", "double", "kW")
 sub1 = h.helicsFederateRegisterSubscription(vfed, "PyDSS.Circuit.heco19021.TotalPower", "")
 #h.helicsInputSetMinimumChange(sub1, 0.1)
-
+print(pub1)
+print(sub1)
 # Enter execution mode #
 h.helicsFederateEnterExecutingMode(vfed)
-
 for t in range(1, 30):
     time_requested = t * 60
     #currenttime = h.helicsFederateRequestTime(vfed, time_requested)
