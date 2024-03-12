c05fd938c6024299d220e4fff886400d18eae070
Latif <aadil.latif@gmail.com>
2024-03-12 13:10:10 -0600

    asd

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -6,6 +6,7 @@ from PyDSS.pydss_project import PyDssProject
 base_path = Path(__file__).parent.absolute()
 
 def test_voltage_ride_through():
+
     pydss_project = base_path / "data" / "controllers"
     project = PyDssProject.load_project(pydss_project)
     PyDssProject.run_project(
