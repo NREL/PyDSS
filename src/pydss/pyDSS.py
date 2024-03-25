30de628d057a91a167c3e85989b8b5fad5ace89d
Latif <aadil.latif@gmail.com>
2024-03-25 12:29:01 -0600

    minor edits

--- src/pydss/pyDSS.py
+++ src/pydss/pyDSS.py
@@ -9,36 +9,6 @@ from pydss.dssInstance import OpenDSS
 from pydss.simulation_input_models import SimulationSettingsModel, dump_settings
 from pydss.utils.utils import dump_data, load_data
 
-
-__author__ = "Aadil Latif"
-__copyright__ = """
-    BSD 3-Clause License
-
-    Copyright (c) 2018, Alliance for Sustainable Energy LLC, All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
-    following conditions are met:
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-    disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
-    following disclaimer in the documentation and/or other materials provided with the distribution.
-    * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote 
-    products derived from this software without specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
-    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-"""
-__license__ = "BSD 3-Clause License"
-__version__ = "2.0.4"
-__maintainer__ = "Aadil Latif"
-__email__ = "aadil.latif@nrel.gov, aadil.latif@gmail.com"
-__status__ = "Production"
-
 PYDSS_BASE_DIR = os.path.join(os.path.dirname(getattr(pydss, "__path__")[0]), "pydss")
 
 class instance(object):
