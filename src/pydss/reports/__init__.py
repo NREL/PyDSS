10f70792ecca6f293ffa577f5adfa09ccf05976a
Latif <aadil.latif@gmail.com>
2024-03-26 10:12:05 -0600

    fixing packaging issue

--- src/pydss/reports/__init__.py
+++ src/pydss/reports/__init__.py
@@ -1,8 +1,8 @@
+from . import *
+# import pkgutil
 
-import pkgutil
 
+# __path__ = pkgutil.extend_path(__path__, __name__)
 
-__path__ = pkgutil.extend_path(__path__, __name__)
-
-for _, modname, _ in pkgutil.walk_packages(path=__path__, prefix=__name__ + '.'):
-    __import__(modname)
+# for _, modname, _ in pkgutil.walk_packages(path=__path__, prefix=__name__ + '.'):
+#     __import__(modname)
